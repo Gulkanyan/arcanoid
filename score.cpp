91b4cdc05800c02430c018b14f60ae3e24c0aed7
Gulkanyan <49342753+papichal@users.noreply.github.com>
2019-05-05 18:32:25 +0400

    Create score.cpp

---
+++ score.cpp
@@ -0,0 +1,26 @@
+#include "score.h"
+#include <QFont>
+
+Score::Score(QGraphicsTextItem *parent) :QGraphicsTextItem(parent)
+{
+    score=0;
+    setPlainText(QString("Score: ")+ QString::number(score));
+    setDefaultTextColor(Qt::cyan);
+    setFont(QFont("times",16));
+
+}
+void Score::increaseScore()
+{
+    score += 50;
+}
+
+int Score::getScore()
+{
+    return score;
+}
+void Score::PaintScore()
+{
+    setPlainText(QString("Score: ")+ QString::number(score));
+    setDefaultTextColor(Qt::cyan);
+    setFont(QFont("times",16));
+}
